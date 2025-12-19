5b6ac2199a16578e0a1bc1e18c2fe92cc9bd2b4e
Aline Brandão <alinep02brandao@gmail.com>
2025-12-18 23:41:37 -0300

    coments sobre tipos de constructor

--- src/Intermediario/Construtores/Hokages.java
+++ src/Intermediario/Construtores/Hokages.java
@@ -6,9 +6,11 @@ public class Hokages {
     int idade;
     boolean vivoOuNao;
 
+    //NOTE: No-Args
     public Hokages() {
     }
 
+    //NOTE: All-Args
     public Hokages (String nome, int idade, boolean vivoOuNao){
         this.nome = nome;
         this.idade = idade;
