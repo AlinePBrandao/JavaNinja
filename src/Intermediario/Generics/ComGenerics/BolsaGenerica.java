17e6682b7d678154e68575edde7f099e8bce32aa
alinebrandao <alinep02brandao@gmail.com>
2026-01-30 14:15:38 -0300

    Exemplo com Generics - BolsaGenerica

--- src/Intermediario/Generics/ComGenerics/BolsaGenerica.java
+++ src/Intermediario/Generics/ComGenerics/BolsaGenerica.java
@@ -1,7 +1,33 @@
 package Intermediario.Generics.ComGenerics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BolsaGenerica <T>{
     //NOTE: para transformar uma classe em genérica adicionar <> e o tipo recebido, T = Type
 
+    private List<T> equipamentos;
 
+    //NOTE: Constructor sem parametro List
+    public BolsaGenerica() {
+        this.equipamentos = new ArrayList<>();
+    }
+
+    public List<T> getEquipamentos() {
+        return equipamentos;
+    }
+
+    public void setEquipamentos(List<T> equipamentos) {
+        this.equipamentos = equipamentos;
+    }
+
+    //NOTE: Colocar equipamentos Genéricos
+    public void adicionarEquipamentos(T equipamento){
+        equipamentos.add(equipamento);
+    }
+
+    @Override
+    public String toString() {
+        return "Bolsa de equipamentos: " + equipamentos.toString();
+    }
 }
