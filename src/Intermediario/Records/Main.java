9804fbc5c6bfa5be9fcc6123108d316915cca334
alinebrandao <alinep02brandao@gmail.com>
2026-01-31 12:03:38 -0300

    Aprendendo Records - Exemplo sem Records

---
+++ src/Intermediario/Records/Main.java
@@ -0,0 +1,9 @@
+package Intermediario.Records;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ninja cadastro = new Ninja("Naruto", "naruto@emailo.com", 99999999);
+        System.out.println("cadastro: " + cadastro);
+    }
+}
