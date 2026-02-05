b0e6638f437979b3789e4062c832ae92ab35db74
alinebrandao <alinep02brandao@gmail.com>
2026-02-05 13:45:22 -0300

    LinkedList - introdução

---
+++ src/Intermediario/LinkedList/Main.java
@@ -0,0 +1,15 @@
+package Intermediario.LinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("Naruto Uzumaki");
+        linkedList.add("Sasuke Uchiha");
+        linkedList.add("Sakura Haruno");
+        System.out.println(linkedList); //NOTE: mostra os elementos da lista
+
+    }
+}
