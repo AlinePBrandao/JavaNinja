dcc67af98deb3bf069cadc7fc83fd91587e7a9bb
alinebrandao <alinep02brandao@gmail.com>
2026-02-05 14:34:13 -0300

    Collection - classes e interface que implementam

--- src/Intermediario/LinkedList/Main.java
+++ src/Intermediario/LinkedList/Main.java
@@ -1,6 +1,6 @@
 package Intermediario.LinkedList;
 
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,5 +16,15 @@ public class Main {
 
         linkedList.remove(2); //NOTE: removendo passando o index
         System.out.println(linkedList);
+
+        //NOTE: Classes e Interfaces que implementam Collections:
+        //NOTE: Classes
+        ArrayList<String> arrayList = new ArrayList<>();
+        LinkedList<String> linkedList1 = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+
+        //NOTE: Interface
+        Queue<String> queue = new LinkedList<>(); //Interface
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(); //Classe que implementa a interface queue
     }
 }
