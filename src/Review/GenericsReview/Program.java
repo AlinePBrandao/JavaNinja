a98d99b7203b110013bf8dc3c8ddbcd515eaf53f
alinebrandao <alinep02brandao@gmail.com>
2026-02-11 15:30:11 -0300

    Review Generics: Classe Program

--- src/Review/GenericsReview/Program.java
+++ src/Review/GenericsReview/Program.java
@@ -1,4 +1,13 @@
 package Review.GenericsReview;
 
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
 }
