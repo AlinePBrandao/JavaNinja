25790c905ee3b33e7cce5cf46583622e554db1bd
alinebrandao <alinep02brandao@gmail.com>
2026-02-11 15:27:37 -0300

    Review Generics: Classe Product

--- src/Review/GenericsReview/Product.java
+++ src/Review/GenericsReview/Product.java
@@ -1,6 +1,27 @@
 package Review.GenericsReview;
 
+import java.util.List;
+
 public class Product <T>{
 
-    private List<T> elementos;
+    private List<T> produtos;
+
+    public Product(List<T> produtos){
+        this.produtos = produtos;
+    }
+    public List<T> getProdutos() {
+        return produtos;
+    }
+    public void setProdutos(List<T> produtos) {
+        this.produtos = produtos;
+    }
+
+    public void adicionarProduto(T produto){
+        produtos.add(produto);
+    }
+
+    @Override
+    public String toString() {
+        return "Produtos: " +  produtos;
+    }
 }
