e0cace00c5e3bccc0c85c9f3dc27532e6508e628
alinebrandao <alinep02brandao@gmail.com>
2026-02-11 17:04:57 -0300

    Review Generics: Product- criação dos atributos, constructor, getters e setters, método toString

--- src/Review/GenericsReview/Product.java
+++ src/Review/GenericsReview/Product.java
@@ -1,7 +1,32 @@
 package Review.GenericsReview;
 
+public class Product{
+    public String nome;
+    public int preco;
 
-public class Product <T>{
+    public Product(String nome, int preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
 
+    public String getNome(){
+        return nome;
+    }
 
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " + nome + " - R$ %.2f%n" + preco;
+    }
 }
