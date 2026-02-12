e07fe583a7b44ff0b0ab8fddfbb9ee6f403653b6
alinebrandao <alinep02brandao@gmail.com>
2026-02-12 18:20:43 -0300

    Review Records: Main - busca por categoria

--- src/Review/RecordsReview/Program.java
+++ src/Review/RecordsReview/Program.java
@@ -1,5 +1,7 @@
 package Review.RecordsReview;
 
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
 
@@ -7,13 +9,18 @@ public class Program {
 
        catalogo.adicionarProduto(new Produto("Lapis", 1.0, "Papelaria"));
        catalogo.adicionarProduto(new Produto("Borracha", 0.5, "Papelaria"));
-       catalogo.adicionarProduto(new Produto("Tinta", 100.0, "Reforma e COnstrução"));
+       catalogo.adicionarProduto(new Produto("Tinta", 100.0, "Reforma e Construção"));
        catalogo.adicionarProduto(new Produto("Ração para Cães", 200.0, "Animais"));
        catalogo.adicionarProduto(new Produto("Esponja", 3.0, "Utilidades do Lar"));
 
        catalogo.exibirProdutos();
 
-
+        System.out.println();
+        System.out.println("Produto na Categoria Papelaria: ");
+        List<Produto> papelaria = catalogo.buscarPorCategoria("Papelaria");
+        for (Produto p : papelaria) {
+            System.out.println(p);
+        }
 
     }
 }
