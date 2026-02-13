0e6cb36619b1130738a592e7c3beb88763a652e8
alinebrandao <alinep02brandao@gmail.com>
2026-02-13 20:50:43 -0300

    Review Records: Biblioteca - Livro: Record Livro criado com atributos e validações no constructor compacto

--- src/Review/RecordsReview/Biblioteca/Livro.java
+++ src/Review/RecordsReview/Biblioteca/Livro.java
@@ -1,4 +1,10 @@
 package Review.RecordsReview.Biblioteca;
 
-public record Livro(){
+public record Livro(String titulo, String autor, int paginas, String categoria) implements ItemBiblioteca {
+    public Livro{
+        if (titulo.isBlank()) throw new IllegalArgumentException("Titulo Inválido");
+        if (autor.isBlank()) throw new IllegalArgumentException("Autor Inválido");
+        if (paginas < 2) throw new IllegalArgumentException("Livro Incompleto");
+        if (categoria.isBlank()) throw new IllegalArgumentException("Categoria Inválida");
+    }
 }
