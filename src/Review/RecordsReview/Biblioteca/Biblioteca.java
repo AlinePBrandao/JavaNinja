616debbbec62b93e4c507c30b44eae5b87215756
alinebrandao <alinep02brandao@gmail.com>
2026-02-13 21:07:29 -0300

    Review Records: Biblioteca - Biblioteca - esqueci de iniciar o ArrayList hehehe

--- src/Review/RecordsReview/Biblioteca/Biblioteca.java
+++ src/Review/RecordsReview/Biblioteca/Biblioteca.java
@@ -1,10 +1,11 @@
 package Review.RecordsReview.Biblioteca;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
 
-    private List<ItemBiblioteca> itens;
+    private List<ItemBiblioteca> itens = new ArrayList<>();
 
     public void adicionarItem(ItemBiblioteca item){
         itens.add(item);
