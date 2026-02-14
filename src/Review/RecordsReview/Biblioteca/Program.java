174ecaecdd2fa234a9aafdd02dfb14d298d490dd
alinebrandao <alinep02brandao@gmail.com>
2026-02-13 21:37:58 -0300

    Review Records: Biblioteca - Main - criada biblioteca instanciando a classe Biblioteca, adicionados 2 livros e 2 revistas

--- src/Review/RecordsReview/Biblioteca/Program.java
+++ src/Review/RecordsReview/Biblioteca/Program.java
@@ -3,5 +3,10 @@ package Review.RecordsReview.Biblioteca;
 public class Program {
     public static void main(String[] args) {
 
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.adicionarItem(new Livro("O Poderoso Chefão", "Mario Puzo", 600, "Ação"));
+        biblioteca.adicionarItem(new Livro("A Hora da Estrela", "Clarice Lispector", 120, "Ficção"));
+        biblioteca.adicionarItem(new Revista("Capricho", 27, "Variedades"));
+        biblioteca.adicionarItem(new Revista("Turma da Mônica", 50, "Historia em Quadrinhos"));
     }
 }
