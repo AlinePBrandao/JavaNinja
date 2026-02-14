af9ff3ae45d8e86835fa9b0f18bb3f0c8a21d5c1
alinebrandao <alinep02brandao@gmail.com>
2026-02-13 21:00:58 -0300

    Review Records: Biblioteca - Revista - criadas variáveis da classe, implementação de ItemBiblioteca com seus métodos sobrescritos

---
+++ src/Review/RecordsReview/Biblioteca/Revista.java
@@ -0,0 +1,19 @@
+package Review.RecordsReview.Biblioteca;
+
+public class Revista implements ItemBiblioteca {
+
+    private String titulo;
+    int edicao;
+    String categoria;
+
+    @Override
+    public String titulo() {
+        return "";
+    }
+
+    @Override
+    public String categoria() {
+        return "";
+    }
+
+}
