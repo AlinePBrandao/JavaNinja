0340c3fc01cb538710aacb99298e45aba28620cb
alinebrandao <alinep02brandao@gmail.com>
2026-02-13 23:12:29 -0300

    Review Stack: Iniciando novo desafio

---
+++ src/Review/StackReview.java
@@ -0,0 +1,20 @@
+package Review;
+
+import java.util.Stack;
+
+public class StackReview {
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Harry");
+        stack.push("Hermione");
+        stack.push("Rone");
+        stack.push("Draco");
+        stack.push("Luna");
+
+        System.out.println("Minha Stack Atual: " + stack);
+
+        System.out.println(stack.pop());
+    }
+}
