c60fdaa824b050fcc6674c0d4d724a13ddd138ad
alinebrandao <alinep02brandao@gmail.com>
2026-02-18 16:15:40 -0300

    Review Stack: Program - switch case 1

--- src/Review/StackReview/Program.java
+++ src/Review/StackReview/Program.java
@@ -7,20 +7,32 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Stack<String> stack = new Stack<>();
+
         System.out.println("=== Menu Undo ===");
         System.out.println("1 - Registrar ação");
         System.out.println("2 - Desfazer última ação");
         System.out.println("3 - Listar ações");
         System.out.println("4 - Sair");
 
-        System.out.println("Escolha: ");
+        System.out.print("Escolha: ");
         int escolha = sc.nextInt();
 
-        
+        switch (escolha) {
+            case 1:
+                if (escolha == 1){
+                    System.out.println("Digite a ação: ");
+                    String escolha1 = sc.nextLine();
+                    stack.push(escolha1);
+                    System.out.println("Ação registrada");
+                }
+                break;
+
+        }
 
 
 
-        Stack<String> stack = new Stack<>();
+
 
 
 
