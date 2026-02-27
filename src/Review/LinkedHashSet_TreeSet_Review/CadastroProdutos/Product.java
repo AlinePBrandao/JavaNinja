5894200eb1126bcaea86a52b2fd2a179cbd0fdfa
alinebrandao <alinep02brandao@gmail.com>
2026-02-27 16:47:13 -0300

    Review LinkedHashSet X TreeSet: iniciando desafio de revisão

---
+++ src/Review/LinkedHashSet_TreeSet_Review/CadastroProdutos/Product.java
@@ -0,0 +1,8 @@
+package Review.LinkedHashSet_TreeSet_Review.CadastroProdutos;
+
+public class Product {
+    private String nome;
+    private double preco;
+
+
+}
---
+++ src/Review/LinkedHashSet_TreeSet_Review/CadastroProdutos/Program.java
@@ -0,0 +1,7 @@
+package Review.LinkedHashSet_TreeSet_Review.CadastroProdutos;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
