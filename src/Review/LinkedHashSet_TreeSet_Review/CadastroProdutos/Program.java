645314fcc866be934c7bd8393af16470fd0f5316
alinebrandao <alinep02brandao@gmail.com>
2026-02-28 13:22:52 -0300

    Review LinkedHashSet X TreeSet: Program - iniciando o menu de opções

--- src/Review/LinkedHashSet_TreeSet_Review/CadastroProdutos/Program.java
+++ src/Review/LinkedHashSet_TreeSet_Review/CadastroProdutos/Program.java
@@ -6,6 +6,16 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int opcao = 0;
+
+        while (opcao != 4){
+            System.out.println("----- Opções -----");
+            System.out.println("1. Cadastrar produto");
+            System.out.println("2. Mostrar produtos (ordem de inserção)");
+            System.out.println("3. Mostrar produtos (ordem ordenada");
+            System.out.println("4. Sair");
+        }
+
         System.out.println("Digite o nome do produto: ");
         String nome = sc.nextLine();
         System.out.println("Digite o valor do produto: ");
