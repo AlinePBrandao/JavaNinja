c6d4e77bd8251c8c066a91faad26639c0bbfe4d9
alinebrandao <alinep02brandao@gmail.com>
2026-03-04 16:13:01 -0300

    Módulo Avançado: BigO - mais exemplos sobre BigO

---
+++ src/Avançado/ComplexidadeAlgoritmos/BigO.java
@@ -0,0 +1,11 @@
+package Avançado.ComplexidadeAlgoritmos;
+
+import java.util.ArrayList;
+
+public class BigO {
+    public static void main(String[] args) {
+
+        ArrayList<Ninjas> ninjas = new ArrayList<>();
+
+    }
+}
---
+++ src/Avançado/ComplexidadeAlgoritmos/Ninjas.java
@@ -0,0 +1,17 @@
+package Avançado.ComplexidadeAlgoritmos;
+
+public class Ninjas {
+
+    String nome;
+    int idade;
+
+    public Ninjas(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
