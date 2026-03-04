261b5556ac491c27d03b03666b517bea3399847f
alinebrandao <alinep02brandao@gmail.com>
2026-03-04 20:35:58 -0300

    Módulo Avançado: BigO - quadrática o(n²)

--- src/Avançado/ComplexidadeAlgoritmos/BigO.java
+++ src/Avançado/ComplexidadeAlgoritmos/BigO.java
@@ -18,9 +18,19 @@ public class BigO {
         System.out.println("----------------------");
 
         //NOTE: Linear o(n), depende do tamanho da lista, quanto maior mais complexa.
+        //NOTE: dica: se tiver uma lista pra percorrer e provavelmente vai demorar = complexidade linear
         for (Ninjas ninja : ninjas){
             System.out.println(ninja);
         }
 
+        System.out.println("----------------------");
+
+        //NOTE: quadratica o(n²)
+        //NOTE: percorrendo com 2 ponteiros - mested loop ou loop encadeado
+        for (int i = 0; i < ninjas.size(); i++ ){
+            for (int j = 0; j < ninjas.size(); j++){
+                System.out.println(ninjas.get(i) + "vs" + ninjas.get(j));
+            }
+        }
     }
 }
