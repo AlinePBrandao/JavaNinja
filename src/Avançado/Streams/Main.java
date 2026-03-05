2a6bc92aa26ae9f7637bd03183d4fbbe8de4378f
alinebrandao <alinep02brandao@gmail.com>
2026-03-05 18:09:10 -0300

    Módulo Avançado: Strams - Main- adicionando nova list

--- src/Avançado/Streams/Main.java
+++ src/Avançado/Streams/Main.java
@@ -1,7 +1,12 @@
 package Avançado.Streams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<NinjaDados> ninjas = new ArrayList<>();
 
     }
 }
