c1c364d67d4cec77360bd1d190e5b706740230b6
alinebrandao <alinep02brandao@gmail.com>
2026-03-05 18:12:14 -0300

    Módulo Avançado: Strams - Main- list populada

--- src/Avançado/Streams/Main.java
+++ src/Avançado/Streams/Main.java
@@ -7,6 +7,13 @@ public class Main {
     public static void main(String[] args) {
 
         List<NinjaDados> ninjas = new ArrayList<>();
+        ninjas.add(new NinjaDados("Naruto Uzumaki", "Konoha", 17));
+        ninjas.add(new NinjaDados("Sasuke Uchiha", "Konoha", 19));
+        ninjas.add(new NinjaDados("Sakura Haruno", "Konoha", 18));
+        ninjas.add(new NinjaDados("Kakashi Hatake", "Konoha", 30));
+        ninjas.add(new NinjaDados("Gaara", "Suna", 20));
+        ninjas.add(new NinjaDados("Temari", "Konoha", 17));
+        ninjas.add(new NinjaDados("Naruto Uzumaki", "Suna", 21));
 
     }
 }
