d562f9d4d1cf3b0d67b3fa9909c7d65abe2d634f
alinebrandao <alinep02brandao@gmail.com>
2026-03-05 22:24:03 -0300

    Módulo Avançado: Strams - Main- método filter filtra a lista recebendo o predicado e o dado referente

--- src/Avançado/Streams/Main.java
+++ src/Avançado/Streams/Main.java
@@ -12,8 +12,14 @@ public class Main {
         ninjas.add(new NinjaDados("Sakura Haruno", "Konoha", 18));
         ninjas.add(new NinjaDados("Kakashi Hatake", "Konoha", 30));
         ninjas.add(new NinjaDados("Gaara", "Suna", 20));
-        ninjas.add(new NinjaDados("Temari", "Konoha", 17));
-        ninjas.add(new NinjaDados("Naruto Uzumaki", "Suna", 21));
+        ninjas.add(new NinjaDados("Temari", "Suna", 17));
+
+        //NOTE: método filter filtra a lista recebendo o predicado e o dado referente
+        ninjas.stream()
+                .filter(ninjaDados -> ninjaDados.getVila().equals("Konoha"))
+                .forEach(System.out::println); //NOTE: soutc
+
+        //NOTE: método sort ordena a lista
 
     }
 }
