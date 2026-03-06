9b2e990f3d8332856b9eb2de2b040a3c634ecf34
alinebrandao <alinep02brandao@gmail.com>
2026-03-06 17:06:47 -0300

    Módulo Avançado: Streams - Main - stream.sorted() ara ordenar a lista

--- src/Avançado/Streams/Main.java
+++ src/Avançado/Streams/Main.java
@@ -13,13 +13,20 @@ public class Main {
         ninjas.add(new NinjaDados("Kakashi Hatake", "Konoha", 30));
         ninjas.add(new NinjaDados("Gaara", "Suna", 20));
         ninjas.add(new NinjaDados("Temari", "Suna", 17));
+        ninjas.add(new NinjaDados("Rock Lee", "Suna", 35));
 
         //NOTE: método filter filtra a lista recebendo o predicado e o dado referente
         ninjas.stream()
                 .filter(ninjaDados -> ninjaDados.getVila().equalsIgnoreCase("konoha"))
                 .forEach(System.out::println); //NOTE: soutc
+        //NOTE: forEach() printa todos os elementos
 
-        //NOTE: método sort ordena a lista
+        System.out.println("------------------------------");
 
+        //NOTE: método sort ordena a lista por comparação
+        //NOTE n1 e n2 tem a funcao de se comparar entre si até que toda a lista tenha sido percorrida e toda ordenação feita
+        ninjas.stream()
+                .sorted((n1, n2) -> Integer.compare(n1.getIdade(), n2.getIdade()))
+                .forEach(System.out::println);
     }
 }
